68a9301a6b7237ec102013c1e2b942b26943bc2a
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2026-08-12 10:36:50 -0400

    chore: drop http direct dependency and simplify network error detection

--- lib/core/errors/network_errors.dart
+++ lib/core/errors/network_errors.dart
@@ -1,10 +1,6 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:http/http.dart' as http;
-
 bool isNetworkError(Object error) {
-  return error is http.ClientException ||
-      error is SocketException ||
-      error is TimeoutException;
+  return error is SocketException || error is TimeoutException;
 }
